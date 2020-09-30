11d015f7f0e57917796b71c419e05a3dcea21768
acongos92 <30706447+acongos92@users.noreply.github.com>
2020-09-29 23:17:08 -0400

    remove pointless code

--- ui.py
+++ ui.py
@@ -8,14 +8,6 @@ __author__ = "Noupin"
 import os
 import sys
 
-#Allow for Python. relative imports
-dir_path = os.path.dirname(os.path.realpath(__file__))
-parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
-sys.path.insert(0, parent_dir_path)
-
-#First Party Imports
-from QTPie import QTPie
-from tunable import Tunable as tunable
 
 
 class UI:
